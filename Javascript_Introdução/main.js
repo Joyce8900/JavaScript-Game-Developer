81f5c3c88bd15c1d955d9226db604e12254b0457
Joyce <joycesantos8900@gmail.com>
2022-09-22 00:28:49 -0300

    Passos com js

--- Javascript_Introdução/main.js
+++ Javascript_Introdução/main.js
@@ -1,5 +1,10 @@
 var nome = "Joyce Santos";
-var idade = 20
-var idade2 = 10
+var n1 = 5;
+var n2 = 4;
+var frase = "Japão é verde e amarelo"
 
-//alert (nome +" tem "+idade+ " anos");
+// alert(nome + " tem " + idade + " anos");
+console.log(n1*n2);
+console.log(nome);
+console.log(frase.replace("Japão","Brasil"))
+// alert((frase.toUpperCase("Japão","Brasil")))
