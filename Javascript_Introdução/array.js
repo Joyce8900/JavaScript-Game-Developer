8f84125e48f82b2531131041a783cb948b14465d
Joyce <joycesantos8900@gmail.com>
2022-09-22 01:02:50 -0300

    Dicionario

---
+++ Javascript_Introdução/array.js
@@ -0,0 +1,25 @@
+
+
+
+var lista = ["maça", "pêra","laranja"];
+console.log(lista);
+console.log(lista[1]);
+lista.push("uva");
+console.log(lista);
+lista.pop();
+console.log(lista[1])
+console.log(lista.length)
+console.log(lista.join(" ,"))
+console.log(lista.reverse())
+console.log(lista.toString())
+
+
+
+
+
+
+
+
+
+
+
